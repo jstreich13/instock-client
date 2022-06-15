e77348c9990e4810e469957121226e7fe3d60fe1
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-06-15 14:03:01 -0400

    mobile version of header complete. might need to readjust active padding

--- instockclient/src/App.js
+++ instockclient/src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav/>
       <Switch>
         {/* <Route path='/' exact component={}/> FOR WAREHOUSE */}
         {/* <Route path='/inventory' exact component={}/> FOR INVENTORY */}

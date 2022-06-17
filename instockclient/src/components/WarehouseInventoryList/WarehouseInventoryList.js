b2348881519d57eb1b01d8cd9fa9862abf2e93ea
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-06-16 23:50:03 -0400

    mobile fontsizes and color styling added

--- instockclient/src/components/WarehouseInventoryList/WarehouseInventoryList.js
+++ instockclient/src/components/WarehouseInventoryList/WarehouseInventoryList.js
@@ -1,43 +1,47 @@
-import React from 'react'
-import './WarehouseInventoryList.scss'
-import inventory from '../../Assets/JSON Data/inventories.json'
-import deleteIcon from '../../Assets/Icons/delete_outline-24px.svg'
-import editIcon from '../../Assets/Icons/edit-24px.svg'
+import React from "react";
+import "./WarehouseInventoryList.scss";
+import inventory from "../../Assets/JSON Data/inventories.json";
+import deleteIcon from "../../Assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../Assets/Icons/edit-24px.svg";
+import arrow from "../../Assets/Icons/chevron_right-24px.svg";
 
 function WarehouseInventoryList() {
-    console.log(inventory);
+  console.log(inventory);
 
   return (
-   <section>
-    {inventory.map(data =>{
-        return(
-            <section>
-            <div key={data.id}>
-                <h4>INVENTORY</h4>
-                <p>{data.itemName}</p>
+    <section className="list">
+      {inventory.map((data) => {
+        return (
+          <section className="list__container">
+            <div className="list__group">
+              <div className="list__inventory-container" key={data.id}>
+                <p className="list__title">INVENTORY ITEM</p>
+                <p className="list__inventory">{data.itemName} <img className="list__item-arrow" src={arrow} alt=""/></p>
+              </div>
+              <div className="list__status-container">
+                <p className="list__title">STATUS</p>
+                <p className="list__status">{data.status}</p>
+              </div>
             </div>
-            <div>
-                <h4>STATUS</h4>
-                <p>{data.status}</p>
+            <div className="list__group">
+              <div className="list__category-container">
+                <p className="list__title">CATEGORY</p>
+                <p className="list__category">{data.category}</p>
+              </div>
+              <div className="list__quantity-container">
+                <p className="list__title">QTY</p>
+                <p className="list__quantity">{data.quantity}</p>
+              </div>
             </div>
-            <div>
-                <h4>CATEGORY</h4>
-                <p>{data.category}</p>
+            <div className="list__image-container">
+              <img className="list__image" src={deleteIcon} alt="Delete Item" />
+              <img className="list__image" src={editIcon} alt="Edit Icon" />
             </div>
-            <div>
-                <h4>QTY</h4>
-                <p>{data.quantity}</p>
-            </div>
-            <div>
-                <img src={deleteIcon} alt='Delete Item'/>
-                <img src={editIcon} alt='Edit Icon'/>
-            </div>
-            </section>
-        )
-    })}
-   </section>
-       
-  )
+          </section>
+        );
+      })}
+    </section>
+  );
 }
 
-export default WarehouseInventoryList
+export default WarehouseInventoryList;

e1703d14e40cec0e164f437689089e742d8f612b
danielj-84 <danieldimatteo123@gmail.com>
2022-06-18 13:47:35 -0400

    finalised modal design for both delete warehouse and delete item

--- instockclient/src/components/DeleteInventoryModal/DeleteInventoryModal.js
+++ instockclient/src/components/DeleteInventoryModal/DeleteInventoryModal.js
@@ -3,17 +3,20 @@ import "./DeleteInventoryModal.scss";
 import close from "../../Assets/Icons/close-24px.svg";
 
 function DeleteInventoryModal(props) {
+
+  const inventoryArr = props.inventoryData.find((item) => item.id === props.deleteId);
+
   return (
     <div className="modal">
       <img
-        className="modal__closewindow" src={close} onClick={props.hideModal} />
+        className="modal__closewindow" src={close} onClick={()=> props.handleModal()} />
       <section className="modal__content">
         <div className="modal__info">
-          <h2 className="modal__title">Delete {props.name} inventory item?</h2>
-          <p className="modal__text"> Please confirm that you'd like to delete {props.name} from the inventory list. You won't be able to undo this action.</p>
+          <h2 className="modal__title">Delete {inventoryArr.name} inventory item?</h2>
+          <p className="modal__text"> Please confirm that you'd like to delete {inventory.name} from the inventory list. You won't be able to undo this action.</p>
         </div>
         <div className="modal__buttons">
-          <button className="modal__cancel" onClick={props.hideModal}>Cancel</button>
+          <button className="modal__cancel" onClick={()=> props.handleModal()}>Cancel</button>
           <button className="modal__delete" onClick={props.delete}>Delete</button>
         </div>
       </section>

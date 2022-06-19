baf32ddea99b631f0aea5932fb3ea8604cf046c6
AliAlhattab <Ali.Alhattab@hotmail.com>
2022-06-19 14:21:10 -0400

    added nav links

--- instockclient/src/components/WarehouseInventoryList/WarehouseInventoryList.js
+++ instockclient/src/components/WarehouseInventoryList/WarehouseInventoryList.js
@@ -5,17 +5,19 @@ import editIcon from "../../Assets/Icons/edit-24px.svg";
 import arrow from "../../Assets/Icons/chevron_right-24px.svg";
 import sortIcon from "../../Assets/Icons/sort-24px.svg";
 import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 
 
 class WarehouseInventoryList extends Component {
 
   state = {
+    id: this.props.warehouseId,
     inventory: []
   }
   
   componentDidMount() {
     axios
-      .get('http://localhost:8080/inventories')
+      .get(`http://localhost:8080/inventories`)
         .then((response) => {
 
           this.setState({
@@ -53,23 +55,20 @@ class WarehouseInventoryList extends Component {
 
         <section className="list">
           {this.state.inventory.map((data) => {
-
-            // if(data.status === 'In Stock' ? green : orange)
-
             return (
               <section className="list__container">
                 <div className="list__group-container">
                   <div className="list__group">
                     <div className="list__inventory-container" key={data.id}>
                       <p className="list__title">INVENTORY ITEM</p>
-                      <p className="list__inventory">
+                      <NavLink to='' className='list__link'><p className="list__inventory">
                         {data.itemName}
                         <img
                           className="list__inventory-arrow"
                           src={arrow}
                           alt="Expand Arrow"
                         />
-                      </p>
+                      </p></NavLink>
                     </div>
                     <div className="list__category-container">
                       <p className="list__title">CATEGORY</p>
@@ -93,7 +92,7 @@ class WarehouseInventoryList extends Component {
                     src={deleteIcon}
                     alt="Delete Item"
                   />
-                  <img className="list__image" src={editIcon} alt="Edit Icon" />
+                  <NavLink className="list__link"to='/'><img className="list__image" src={editIcon} alt="Edit Icon" /></NavLink>
                 </div>
               </section>
             );
